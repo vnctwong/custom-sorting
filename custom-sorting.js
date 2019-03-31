ac37941c2a1f672a99a440efe3026f7f43625281
vnctwong <vincentkywong@gmail.com>
2019-03-31 04:52:16 +0000

    sorted by name then age

--- custom-sorting.js
+++ custom-sorting.js
@@ -6,21 +6,36 @@
 // console.log(array1)
 
 //sort alphabetically and then by descending age
-var students = [
-  { id: 1, name: "bruce",    age: 40 },
-  { id: 2, name: "zoidberg", age: 22 },
-  { id: 3, name: "alex",     age: 22 },
-  { id: 4, name: "alex",     age: 30 }
+var students = [{
+    id: 1,
+    name: "bruce",
+    age: 40
+  },
+  {
+    id: 2,
+    name: "zoidberg",
+    age: 22
+  },
+  {
+    id: 3,
+    name: "alex",
+    age: 22
+  },
+  {
+    id: 4,
+    name: "alex",
+    age: 30
+  }
 ];
 
-  students.sort(function (a, b) {
+students.sort(function (a, b) {
 
   if (a.name < b.name) {
     return -1;
   } else if (a.name > b.name) {
     return 1;
   } else if (a.name === b.name) {
-    return 1;
+    return b.age - a.age;
   }
 });
 
@@ -35,9 +50,4 @@ var students = [
 //older first = larger first = >0
 
 console.log(students);
-
-
-
-
-
-
+//SOLVED BUT HOW TO PRINT
